ecf71fb3cc3758b96b0f7e9aa4ef100de50c7e81
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com.br>
2026-06-23 22:18:26 -0300

    Add: Terminando de navegar no drive. Resolvido problema com jupyter

--- index.py
+++ index.py
@@ -5,13 +5,21 @@ pyautogui.PAUSE = 0.5
 
 link = 'https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga'
 
+# entrar no (sistema)
 pyautogui.press('win')
 pyautogui.write('chrome')
 pyautogui.press('enter')
 time.sleep(1)
-pyautogui.moveTo(650, 1255)
-pyautogui.click()
+pyautogui.click(x=650, y=1255, clicks=1)
 pyautogui.write(link)
 pyautogui.press('enter')
-time.sleep(3)
-pyautogui.hotkey('alt', 'f4')
+
+# Navegar no Sistema
+time.sleep(1)
+pyautogui.click(x=425, y=450, clicks=2)
+pyautogui.press('f11')
+time.sleep(1.4)
+pyautogui.click(x=2328, y=304, clicks=1)
+time.sleep(4)
+pyautogui.click(x=1004, y=734, clicks=1)
+pyautogui.press('f11')
