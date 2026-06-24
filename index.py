8165feb69d52e7872a071c0261efe8f844eb3858
Guilherme Lima e Silva <guilhermelimaesilva.p@gmail.com.br>
2026-06-23 21:43:35 -0300

    Add: Adicionando lógica inicial do Pyautogui, dificuldade em usar o jupyter com pyautogui. Mas no python puro tudo ok

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-print("Hello World!")
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.5
+
+link = 'https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga'
+
+pyautogui.press('win')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.moveTo(650, 1255)
+pyautogui.click()
+pyautogui.write(link)
+pyautogui.press('enter')
+time.sleep(3)
+pyautogui.hotkey('alt', 'f4')
